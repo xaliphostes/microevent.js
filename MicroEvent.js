38d1a9b22d460d3c0cac881e86b968dd72ec8039
Xaliphostes <xaliphostes@users.noreply.github.com>
2019-05-28 19:56:47 +0200

    indentation

--- MicroEvent.js
+++ MicroEvent.js
@@ -7,45 +7,45 @@
  */
 
 class MicroEvent {
-	constructor() {
-		this._events = {}
-	}
-	bind(event, fct) {
-		this._events[event] = this._events[event] || []
-		this._events[event].push(fct)
-	}
+    constructor() {
+        this._events = {}
+    }
+    bind(event, fct) {
+        this._events[event] = this._events[event] || []
+        this._events[event].push(fct)
+    }
 
-	unbind(event, fct) {
-		if (event in this._events === false) {
-			return
-		}
-		this._events[event].splice(this._events[event].indexOf(fct), 1)
-	}
-	
-	trigger(event) {
-		if (event in this._events === false) {
-			return
-		}
-		this._events.forEach( e => e.apply(this, Array.prototype.slice.call(arguments, 1)) )
-	}
+    unbind(event, fct) {
+        if (event in this._events === false) {
+            return
+        }
+        this._events[event].splice(this._events[event].indexOf(fct), 1)
+    }
+    
+    trigger(event) {
+        if (event in this._events === false) {
+            return
+        }
+        this._events.forEach( e => e.apply(this, Array.prototype.slice.call(arguments, 1)) )
+    }
 
-	/**
-	 * mixin will delegate all MicroEvent.js function in the destination object
-	 * - require('MicroEvent').mixin(Foobar) will make Foobar able to use MicroEvent
-	 * @param {Object} the object which will support MicroEvent
+    /**
+     * mixin will delegate all MicroEvent.js function in the destination object
+     * - require('MicroEvent').mixin(Foobar) will make Foobar able to use MicroEvent
+     * @param {Object} the object which will support MicroEvent
      * @see MicroEventDecorator in ../decorators/MicroEventDecorator
-	 */
-	static mixin(destObject) {
+     */
+    static mixin(destObject) {
         const props	= ['bind', 'unbind', 'trigger']
         props.forEach( prop => {
             if( typeof destObject === 'function' ) {
-				destObject.prototype[prop]	= MicroEvent.prototype[prop]
-			} else {
-				destObject[prop] = MicroEvent.prototype[prop]
-			}
+                destObject.prototype[prop]	= MicroEvent.prototype[prop]
+            } else {
+                destObject[prop] = MicroEvent.prototype[prop]
+            }
         })
-		return destObject
-	}
+        return destObject
+    }
 }
 
 export default MicroEvent
